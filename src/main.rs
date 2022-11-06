d60682586536aabb52a2694cbaae77e7a4130501
smallB007 <atch.cpp@gmail.com>
2022-11-06 10:48:43 +0100

    Creating classic_layout

--- src/main.rs
+++ src/main.rs
@@ -25,26 +25,14 @@ use cursive_table_view::{TableView, TableViewItem};
 
 mod tui_fn;
 use tui_fn::{
-    create_classic_buttons::create_classic_buttons, create_menu, create_panel::create_panel,
+    create_classic_buttons::create_classic_buttons, create_classic_layout::create_classic_layout,
+    create_menu::create_menubar, create_panel::create_panel,
 };
 fn main() {
     let mut siv = cursive::default();
-    create_menu::create_menubar(&mut siv);
-    let mut layout_panes = LinearLayout::new(Orientation::Horizontal);
-
-    layout_panes.add_child(create_panel("Left", "LeftDialog"));
-    layout_panes.add_child(create_panel("Right", "RightDialog"));
-
-    let layout_circular_panes = CircularFocus::new(layout_panes);
-    let layout_circular_panes = layout_circular_panes.wrap_tab();
-
-    //  let aligned_center = AlignedView::with_center(stack_buttons);
-    let classic_buttons = create_classic_buttons();
-    let main_view = LinearLayout::vertical()
-        .child(layout_circular_panes)
-        .child(classic_buttons);
-
-    siv.add_fullscreen_layer(main_view);
+    create_menubar(&mut siv);
+    let classic_layout = create_classic_layout();
+    siv.add_fullscreen_layer(classic_layout);
 
     siv.run();
 }
--- src/tui_fn/mod.rs
+++ src/tui_fn/mod.rs
@@ -1,4 +1,5 @@
 pub mod create_classic_buttons;
+pub mod create_classic_layout;
 pub mod create_menu;
 pub mod create_panel;
 pub mod create_table;
