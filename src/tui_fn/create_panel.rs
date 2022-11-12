173dd7bcb7893ee14cfeb8a87ef4f8023b861c85
smallB007 <atch.cpp@gmail.com>
2022-11-12 15:44:51 +0100

    Traversing up

--- src/tui_fn/create_panel.rs
+++ src/tui_fn/create_panel.rs
@@ -2,7 +2,10 @@ use std::path::PathBuf;
 
 use crate::cursive::view::{Nameable, Resizable};
 use crate::tui_fn::create_table::{create_table, prepare_items_for_table_view};
-use cursive::views::{Dialog, HideableView, NamedView, ResizedView};
+use cursive::{
+    views::{Dialog, HideableView, NamedView, ResizedView},
+    Cursive,
+};
 use cursive_table_view::TableView;
 
 use super::create_table::{BasicColumn, DirView};
@@ -10,18 +13,65 @@ use super::create_table::{BasicColumn, DirView};
 fn create_name_for_table_view(name: &str) -> String {
     String::from(String::from(name) + "_tableview")
 }
+fn get_current_path_from_dialog_name(s: &mut Cursive, dialog_name: String) -> String {
+    /*First get the dialog's title which is first path of dir */
+    let current_path = s
+        .call_on_name(&dialog_name, |s: &mut Dialog| {
+            let title = String::from(s.get_title());
+            title
+        })
+        .unwrap();
+
+    current_path
+}
+fn traverse_up(s: &mut Cursive, dialog_name: String, table_view_name: String) {
+    /*Third, combine them to form full path */
+    let current_path = get_current_path_from_dialog_name(s, dialog_name.clone());
+    let mut full_path = PathBuf::from(current_path);
+    full_path.pop();
+    if full_path.is_dir() {
+        let new_dialog_title = full_path.into_os_string().into_string().unwrap();
+        s.call_on_name(
+            &table_view_name,
+            |table: &mut TableView<DirView, BasicColumn>| {
+                let items = prepare_items_for_table_view(&new_dialog_title);
+                table.set_items(items);
+            },
+        );
+        s.call_on_name(&dialog_name, |s: &mut Dialog| {
+            s.set_title(new_dialog_title);
+        });
+    }
+}
+fn traverse_down(
+    s: &mut Cursive,
+    dialog_name: String,
+    table_view_name: String,
+    selected_item: String,
+) {
+    /*Third, combine them to form full path */
+    let current_path = get_current_path_from_dialog_name(s, dialog_name.clone());
+    let full_path = PathBuf::from(current_path.clone() + &selected_item);
+    if full_path.is_dir() {
+        let new_dialog_title = String::from(current_path + &selected_item);
+        s.call_on_name(
+            &table_view_name,
+            |table: &mut TableView<DirView, BasicColumn>| {
+                let items = prepare_items_for_table_view(&new_dialog_title);
+                //table.remove_item(index);
+                table.set_items(items);
+            },
+        );
+        s.call_on_name(&dialog_name, |s: &mut Dialog| {
+            s.set_title(new_dialog_title);
+        });
+    }
+}
 pub fn create_panel(name: &str, dir: &str) -> ResizedView<NamedView<Dialog>> {
     let table_view_name = create_name_for_table_view(name);
-    let dialog_title = String::from(name);
+    let dialog_name = String::from(name);
     let table_view = create_table(dir); //.with_name(String::from(name) + "_tableview");
     let table_view = table_view.on_submit(move |s, r, index| {
-        /*First get the dialog's title which is first path of dir */
-        let path = s
-            .call_on_name(&dialog_title, |s: &mut Dialog| {
-                let title = String::from(s.get_title());
-                title
-            })
-            .unwrap();
         /*Second get the selected item */
         let selected_item = s
             .call_on_name(
@@ -32,22 +82,15 @@ pub fn create_panel(name: &str, dir: &str) -> ResizedView<NamedView<Dialog>> {
                 },
             )
             .unwrap();
-        /*Third, combine them to form full path */
-
-        let full_path = PathBuf::from(path.clone() + &selected_item);
-        if full_path.is_dir() {
-            let new_dialog_title = String::from(path + &selected_item);
-            s.call_on_name(
-                &table_view_name,
-                |table: &mut TableView<DirView, BasicColumn>| {
-                    let items = prepare_items_for_table_view(&new_dialog_title);
-                    //table.remove_item(index);
-                    table.set_items(items);
-                },
+        if selected_item == ".." {
+            traverse_up(s, dialog_name.clone(), table_view_name.clone());
+        } else {
+            traverse_down(
+                s,
+                dialog_name.clone(),
+                table_view_name.clone(),
+                selected_item.clone(),
             );
-            s.call_on_name(&dialog_title, |s: &mut Dialog| {
-                s.set_title(new_dialog_title);
-            });
         }
     });
     let table_view = table_view.with_name(create_name_for_table_view(name));
